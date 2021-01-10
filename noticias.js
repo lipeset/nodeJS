17a40ba31b8ea6aae77bac850508d0e23f12b93d
Felipe <felipea.lucas@outlook.com>
2021-01-10 15:00:43 -0300

    aula_8

---
+++ noticias.js
@@ -0,0 +1,5 @@
+console.log('Criando um site de notícias com NodeJS')
+/*console.log() - usado para imprimir uma mensagem no shell utilizado.
+Para rodar o arquivo de extensão .js, é necessário utilziar o comando
+'node nome_do_arquivo', para que o mesmo seja executado.*/
+
