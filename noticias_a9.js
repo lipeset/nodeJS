b042c08789689a58a345d21cbc6ce7eea18f787a
Felipe <felipea.lucas@outlook.com>
2021-01-10 15:34:21 -0300

    aula_9

--- noticias_a9.js
+++ noticias_a9.js
@@ -1,47 +1,36 @@
-console.log('Criando um site de notícias com NodeJS')
-/*console.log() - usado para imprimir uma mensagem no shell utilizado.
-Para rodar o arquivo de extensão .js, é necessário utilziar o comando
-'node nome_do_arquivo', para que o mesmo seja executado.*/
-
-/*Mas como podemos 'escutar' requisições HTTP e fornecer informações
-para alguém que faz uma request via browser?
-
-Basicamente, precisamos começar importando a biblioteca do NodeJS
-chamada de 'HTTP'. A partir deste biblioteca, nós podemos criar um 
-servidor e com isso, passar a escutar requisições feitas em uma
-porta específica. Então quando algum browser disparar uma requisição
-para a nossa máquina naquela porta, nós seremos capazes de recuperar
-essa requisição e fornecer uma resposta.*/
-
-//------------------------------------------------------//
-
-/*require() - Função JavaScript que permite incorporar outros 
-arquivos ao nosso arquivo atual. Somos capazes de incorporar um 
-módulo, uma biblioteca, outras páginas, etc. Em resumo, usamos 
-para acrescentar alguma coisa ao nosso script. No caso, estamos 
-incorporando a biblioteca HTTP.*/
 var http = require('http');
 
-/*createServer() - Vindo da biblioteca HTTP. Método utilizado para
-subir um servidor. Quando se trabalha com NodeJS, muitas vezes
-passamos funções como argumentos dos métodos, neste caso, não será
-diferente.
-Variável REQ = recebe a requisição
-Variável RES = envia a resposta
-Método .END() = é a resposta enviada ao servidor
-Porém, isso ainda não é o suficiente, temos de adicionar qual a
-porta que nosso servidor irá responder.
-Isso pode ser feito de duas formas: adicionando o médoto
-.LISTEN(PORTA) ao final da nossa função, ou como utilizamos abaixo,
-atribuindo o servidor a uma variável e utilizando o .LISTEN()
-separado*/
+/*Como visto no arquivo noticias_a8, nosso servidor não é capaz de
+entender o que está vindo das URLS, de tal modo que, quando utilizamos
+outro endereço (localhost:3000/exemplo), é retornado a mesma response.
+Veremos agora como podemos mapear as URLS e responder de acordo com
+cada solicitação.
+Variável CATEGORIA = captura a URL solicitada e a utilizamos para 
+checar qual response será enviado, da forma correta.*/
 var server = http.createServer(function(req, res){
-    res.end("<html><body>Portal de notícias</body></html>")
-});
+    var categoria = req.url;
 
-/*listen() = Utilizado para definir qual a porta padrão nosso 
-servidor irá responder. Dessa forma, basta rodar o comando
-node nome_arquivo no shell de comando e enviar uma request ao
-endereço LOCALHOST:3000, irá receber a resposta contida na variável
-server (res.end(resposta)).*/
+    if (categoria == '/tecnologia'){
+        res.end("<html><body>Notícias de tecnologia</body></html>");
+    } else if (categoria == '/moda'){
+        res.end("<html><body>Notícias de moda</body></html>");
+    } else if (categoria == '/beleza'){
+        res.end("<html><body>Notícias de beleza</body></html>");
+    } else {
+        res.end("<html><body>Portal de Notícias</body></html>");
+    }
+});
 server.listen(3000);
+
+/*Da forma que montamos acima, conseguimos mapear manualmente, quais as
+URLS existentes e responder de acordo com a solicitação. Embora essa não
+seja a melhor maneira de faze-lo, também é possível dessa forma.
+
+Também é importante lembrar que é necessário derrubar o servidor e
+executa-lo novamente. Embora o JavaScript seja uma linguagem interpretada,
+quando nós ativamos uma instância do NodeJS, ele recupera todos os arquivos
+naquele estado e cria um processo dentro do NodeJS. Então toda alteração
+que é feita, após o servidor estar funcionando, é necessário que seja
+reiniciado o servidor. No nosso caso, não estamos trabalhando com threads
+e sim com forks, ou seja, um processo dentro do próprio NodeJS, por isso
+é necessário sua reinicialização.*/
