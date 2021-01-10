89fc23dd762b3c636290c37663846b85ff75c1c4
Felipe <felipea.lucas@outlook.com>
2021-01-10 19:25:22 -0300

    ejs_refactor

---
+++ app_3.js
@@ -0,0 +1,23 @@
+/*Agora vamos refatorar o arquivo "app_2" utilizando as novas views criadas
+juntamente com os recursos do EJS+EXPRESS*/
+
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render("home/index");
+});
+
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.send("admin/form_add_noticia");
+});
+
+app.get('/noticias', function(req, res){
+    res.send("noticias/noticias");
+});
+
+app.listen(3000, function(){
+    console.log("Servidor rodando com express");
+});
