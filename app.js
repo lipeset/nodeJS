18302e7458b954e21e823fbd5c4209b8498a69d3
Felipe <felipea.lucas@outlook.com>
2021-01-16 16:18:11 -0300

    refact_error

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var app = require('./config/server');
 /*Agora vamos incluir todos os módulos de rotas dentro da nossa 
 aplicação.
 Primeiro, vamos o REQUIRE() padrão para trazer para dentro do nosso
-arquivo, o módulo HOME. Lembrando que quando executamos um REQUIRE()
+arquivo, o módulo NOTICIAS. Lembrando que quando executamos um REQUIRE()
 onde o retorno é uma função, não precisamos executá-lo de imedaito,
 podemos fazer a sua execução em outro momento... Desta forma,
 conseguimos passar para ele um parâmetro, que é a nossa intenção nesse
 momento.*/
-var rotaHome = require('./app/routes/home');
-rotaHome(app);
+var rotaNoticias = require('./app/routes/noticias');
+rotaNoticias(app);
 
-app.listen(function(){
+app.listen(3000, function(){
     console.log('Servidor ON');
 });
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,5 @@
-const app = require("../../config/server");
-
-app.get('/noticia', function(req,res){
-    res.render("noticias/noticias");
-});
+module.exports = function(app){
+    app.get('/noticias', function(req,res){
+        res.render("noticias/noticias");
+    });
+};
