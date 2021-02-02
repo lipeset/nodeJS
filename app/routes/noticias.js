b9daaf76d57fc81215fe2ef4f866072edb02a943
Felipe <felipea.lucas@outlook.com>
2021-02-02 11:03:45 -0300

    noticia_details

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -65,7 +65,7 @@ module.exports = function (app) {
     app.get('/noticias', function (req, res) {
         var connection = app.config.dbConnection();
         connection.query('select * from noticias', function (error, result) {
-            res.render("noticias/noticias", {noticia : result});
+            res.render("noticias/noticias", {noticias : result});
         });
     });
 };
