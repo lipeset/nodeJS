16d8254474eae011f9f09492a93ef6479d2f80ba
Felipe <felipea.lucas@outlook.com>
2021-02-04 09:06:54 -0300

    form_construct

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -19,4 +19,8 @@ module.exports = function(app){
     app.get('/formulario_inclusao_noticia', function(req,res){
         res.render("admin/form_add_noticia");
     });
+
+    app.post('/noticias/salvar', function(req,res){
+        res.send('Chegou na página');
+    });
 };
