656a2f2df55152246fa1c25219f94347f217cca4
Felipe <felipea.lucas@outlook.com>
2021-02-09 13:18:20 -0300

    body-parser_using

--- config/server.js
+++ config/server.js
@@ -38,16 +38,18 @@ no arquivo 'app.js', deixando esse trabalho agora por conta do CONSIGN.*/
 
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
 
 var app = express();
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 consign()
     .include('app/routes')
     .then('config/dbConnection.js')
     .then('app/models')
     .into(app);
-
-app.set('view engine', 'ejs');
-app.set('views', './app/views');
 
 module.exports = app;
