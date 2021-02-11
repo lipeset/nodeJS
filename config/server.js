5aef93b979d04a0d4cdafa829b690862734eda8d
Felipe <felipea.lucas@outlook.com>
2021-02-11 08:56:44 -0300

    express-validator_install

--- config/server.js
+++ config/server.js
@@ -39,12 +39,14 @@ no arquivo 'app.js', deixando esse trabalho agora por conta do CONSIGN.*/
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 consign()
     .include('app/routes')
