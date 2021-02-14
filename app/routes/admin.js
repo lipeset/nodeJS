2e07ad26fc3c50c4e71c85512f5f4ddd8eeb38a4
Felipe <felipea.lucas@outlook.com>
2021-02-14 10:45:37 -0300

    add_controllers

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -17,31 +17,10 @@ o nosso módulo, nós vamos precisar passar este parâmetro para ele.*/
 
 module.exports = function(apply){
     apply.get('/formulario_inclusao_noticia', function(req,res){
-        res.render("admin/form_add_noticia", {validacao : {}, noticia : {}});
+        apply.app.controllers.admin.formulario_inclusao_noticia(apply, req, res);
     });
 
     apply.post('/noticias/salvar', function(req,res){
-        var noticia = req.body;
-
-        req.assert('titulo','Título é obrigatório.').notEmpty();
-        req.assert('resumo','Resumo é obrigatório.').notEmpty();
-        req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres.').len(10,100);
-        req.assert('autor','Autor é obrigatório.').notEmpty();
-        req.assert('data_noticia','Data é obrigatória.').notEmpty().isDate({format: 'YYYY-MM-DD'});
-        req.assert('noticia','Notícia é obrigatória.').notEmpty();
-
-        var erros = req.validationErrors();
-
-        if(erros){
-            res.render("admin/form_add_noticia", {validacao: erros, noticia: noticia});
-            return;
-        }
-
-        var connection = apply.config.dbConnection();
-        var noticiasModel = new apply.app.models.NoticiasDAO(connection);
-        
-        noticiasModel.salvarNoticia(noticia, function (error, result) {
-            res.redirect('/noticias');
-        });
+        apply.app.controllers.admin.noticias_salvar(apply, req, res);
     });
 };
