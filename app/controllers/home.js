a9150c909d78bed8b719ebc2c55a7cb456e945b2
Felipe <felipea.lucas@outlook.com>
2021-02-23 12:57:56 -0300

    literaly_object_from_ejs

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,9 @@
-module.exports.index = function(apply, req, res){
-    res.render("home/index");
+module.exports.index = function (apply, req, res) {
+
+    var connection = apply.config.dbConnection();
+    var noticiasModel = new apply.app.models.NoticiasDAO(connection);
+
+    noticiasModel.get5UltiamsNoticias(function (error, result) {
+        res.render("home/index", { noticias: result });
+    });
 };
--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -1,19 +1,23 @@
-function NoticiasDAO(connection){
+function NoticiasDAO(connection) {
     this._connection = connection;
 }
 
-NoticiasDAO.prototype.getNoticias = function(callback){
+NoticiasDAO.prototype.getNoticias = function (callback) {
     this._connection.query('select * from noticias', callback);
 }
 
-NoticiasDAO.prototype.getNoticia = function(callback){
+NoticiasDAO.prototype.getNoticia = function (callback) {
     this._connection.query('select * from noticias where id_noticia = 2', callback);
 }
 
-NoticiasDAO.prototype.salvarNoticia = function(noticia, callback){
+NoticiasDAO.prototype.salvarNoticia = function (noticia, callback) {
     this._connection.query('insert into noticias set ? ', noticia, callback)
 }
 
-module.exports = function(){
+NoticiasDAO.prototype.get5UltiamsNoticias = function (callback) {
+    this._connection.query('select * from noticias order by data_criacao desc limit 5', callback)
+}
+
+module.exports = function () {
     return NoticiasDAO;
 };
